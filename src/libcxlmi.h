0561b984f004f13900e65a1edc948b1503a9bee8
Anisa Su <anisa.su@samsung.com>
2024-12-03 01:40:24 +0000

    fmapi: add command initiate dc add (5604h)
    Signed-off-by: Anisa Su <anisa.su@samsung.com>

--- src/libcxlmi.h
+++ src/libcxlmi.h
@@ -604,6 +604,11 @@ int cxlmi_cmd_fmapi_get_dc_reg_ext_list(struct cxlmi_endpoint *ep,
 				struct cxlmi_tunnel_info *ti,
 				struct cxlmi_cmd_fmapi_get_dc_reg_ext_list_req *in,
 				struct cxlmi_cmd_fmapi_get_dc_reg_ext_list_rsp *ret);
+
+int cxlmi_cmd_fmapi_initiate_dc_add(struct cxlmi_endpoint *ep,
+				struct cxlmi_tunnel_info *ti,
+				struct cxlmi_cmd_fmapi_initiate_dc_add_req *in);
+
 #ifdef __cplusplus
 }
 #endif
